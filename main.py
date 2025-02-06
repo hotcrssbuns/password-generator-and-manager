f2691ba6c433ff232b1d44bd274f1511bdb04e8f
“Eren” <135561434+hotcrssbuns@users.noreply.github.com>
2025-02-06 15:47:47 -0600

    Added more files to file structure

---
+++ main.py
@@ -0,0 +1,13 @@
+from ultils import clear_screen
+
+
+def main():
+    while True:
+        print("Welcome to hotcrssbuns' Password Manager!")
+        print("\n1. Generate new password")
+        print("2. View saved passwords")
+        print("3. Exit")
+        choice = input("\nChoose an option (1-3): ")
+
+
+main()
---
+++ ui.py
@@ -0,0 +1,3 @@
+class Command_parser:
+    def __init__(self):
+        pass
